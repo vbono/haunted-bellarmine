f0b63f1d805942333a5861de73011ca8701d1713
jkramerDev <jkramer@fairfield.edu>
2022-08-18 09:32:24 -0400

    Initial commit

---
+++ App.java
@@ -0,0 +1,20 @@
+
+public class App {
+
+	public static void main(String[] args) {
+		App app = new App();
+		app.process();
+
+	}
+
+	/**
+	 * main interactive loop of program
+	 * 
+	 */
+
+	public void process() {
+
+		System.out.println("-- let's play! -- ");
+
+	}
+}
