6c0fe5e8f78863f267951c6bb54e66252583edd2
vbono <80070331+vbono@users.noreply.github.com>
2022-09-21 17:56:51 -0400

    help me

--- Help.java
+++ Help.java
@@ -1,7 +1,15 @@
-package milestone1package;
+package g1s1Package;
 
 public class Help {
-  
-  system.out.println("hey");
+	
+	public void HelpMe() {
+		
+		System.out.println("I know everything. Well, I know a lot things. Like places, actions, and objects. To move\n"
+				+ "try keywords like EAST, WEST, NORTH, SOUTH, UP, DOWN, ENTER, etc. Some objects require\n"
+				+ "actions like TAKE, DROP, USE. Type INVENTORY or INV to check your inventory. \n"
+				+ "Good luck!");
+		// add more things as u go
+	}
+
 
 }
