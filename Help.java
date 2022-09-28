c0e30bd4aaf4f6d294bcdc3801582fe343c22865
amdidier <113067294+amdidier@users.noreply.github.com>
2022-09-28 17:42:09 -0400

    includes maps

--- Help.java
+++ Help.java
@@ -1,15 +1,52 @@
-package g1s1Package;
+package milestone1package;
 
 public class Help {
-	
-	public void HelpMe() {
+
+	public void helpMe() {
 		
 		System.out.println("I know everything. Well, I know a lot things. Like places, actions, and objects. To move\n"
 				+ "try keywords like EAST, WEST, NORTH, SOUTH, UP, DOWN, ENTER, etc. Some objects require\n"
 				+ "actions like TAKE, DROP, USE. Type INVENTORY or INV to check your inventory. \n"
-				+ "Good luck!");
+				+ "Good luck! \n"
+				+ "type MAP for a map");
+		
+	}
+	
+	public void map() {
+		System.out.println("");
+		System.out.println("Here's a map to help you navigate!");
+		System.out.println("Basement:");
+		System.out.println("_________________________________________________");
+		System.out.println("|                      |                         |");
+		System.out.println("|       storage        |                         |");
+		System.out.println("|         room         |           art           |");
+		System.out.println("|                      |          museum         |");
+		System.out.println("|______________________|                         |");
+		System.out.println("|                      |                         |");
+		System.out.println("|                      |                         |");
+		System.out.println("|        dungeon       |                         |");
+		System.out.println("|                      |   | stairs |            |");
+		System.out.println("|______________________|_________________________|");
+		
+		System.out.println(" ");
+		System.out.println("Floor 1:");
+		System.out.println("_________________________________________________");
+		System.out.println("|                                                |");
+		System.out.println("|                                                |");
+		System.out.println("|                  great hall                    |");
+		System.out.println("|                                                |");
+		System.out.println("|________________________________________________|");
+		System.out.println("|                      |                         |");
+		System.out.println("|      president's     |    hall of presidents   |");
+		System.out.println("|         study        |                         |");
+		System.out.println("|                      |   | stairs |            |         *");
+		System.out.println("|______________________|_________________________|        * *");
+		System.out.println("                    |     |");
+		System.out.println("                   /       \\ ");
+		System.out.println("                  (         ) ");
+		System.out.println("                   \\       /  ");
+		
 		// add more things as u go
 	}
-
 
 }
