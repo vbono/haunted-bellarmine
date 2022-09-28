ed46a5e232fb69efeba80a9ba690f87c319529a5
vbono <80070331+vbono@users.noreply.github.com>
2022-09-28 18:04:52 -0400

    user

---
+++ User.java
@@ -0,0 +1,17 @@
+package g1s1Package;
+import java.util.Scanner;
+
+public class User {
+	Scanner myScanner = new Scanner(System.in);
+	Location loc = new Location();
+	
+	public void currentLocation() {
+		
+		
+		
+	}
+	
+	
+	
+
+}
