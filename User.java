50450a5c3abc8180f71666ca82a49dcbb67763b9
vbono <80070331+vbono@users.noreply.github.com>
2022-10-05 12:02:30 -0400

    USER ABLE TO MOVE!!!!!!!!!!!!!!!!!!

--- User.java
+++ User.java
@@ -1,29 +1,316 @@
 package g1s1Package;
+import java.util.Scanner;
 
-public class User {
-	private String name;
-	private Location loc;
+public class Location {
+	Scanner s = new Scanner(System.in);
 	
-	public User() {
-		loc = new Location();
+	public String currentLocation;
+	private static final String START = "start";
+	private static final String FOREST = "forest";
+	private static final String ZEN_GARDEN = "zen garden";
+	private static final String MANSION = "mansion";
+	private static final String STUDY = "study";
+	private static final String GREAT_HALL = "great hall";
+	private static final String ROOM = "room";
+	private static final String PRESIDENTS_HALL = "presidents hall";
+	private static final String DOWNSTAIRS = "downstairs";
+	private static final String ART_MUSEUM = "art museum";
+	private static final String STORAGE = "storage room";
+	private static final String DUNGEON = "dungeon";
+	private static final String EAST = "east";
+	private static final String WEST = "west";
+	private static final String NORTH = "north";	
+	private static final String SOUTH = "south";
+	private static final String STAIRS = "stairs";
+	private static final String BACK = "back";
+	private static final String UPSTAIRS = "upstairs";
+	
+	public Location() {
+		currentLocation = START;
 	}
 	
-	public String getLocation() {
-		return loc.currentLocation();
+	public String currentLocation() {
+		return currentLocation;
+		
 	}
 	
 	public void move(String direction) {
-		loc.move(direction);
+		
+		switch (currentLocation) {
+			case START:
+				startMove(direction);
+				break;
+			case MANSION:
+				mansionMove(direction);
+				break;
+			case FOREST:
+				forestMove(direction);
+				break;
+			case ZEN_GARDEN:
+				zenGMove(direction);
+				break;
+			case STUDY:
+				studyMove(direction);
+				break;
+			case GREAT_HALL:
+				greatHMove(direction);
+				break;
+			case PRESIDENTS_HALL:
+				presidentsHMove(direction);
+				break;
+			case ART_MUSEUM:
+				museumMove(direction);
+				break;
+			case STORAGE:
+				storageMove(direction);
+				break;
+			case DUNGEON:
+				dungeonMove(direction);
+				break;
+			default:
+				System.out.println("Did you type your word correctly?");
+		}
 	}
 	
-	public String getName() {
-		return name;
+	private void startMove(String direction) {
+		
+		switch (direction) {
+			case MANSION:
+				currentLocation = MANSION;
+				break;
+			case FOREST:
+				currentLocation = FOREST;
+				break;
+		}
 	}
 	
-	public void setName(String name) {
-		this.name=name;
+	private void mansionMove(String direction) {
+		
+		switch (direction) {
+			case EAST:
+				currentLocation = PRESIDENTS_HALL;
+				break;
+			case ROOM:
+				currentLocation = PRESIDENTS_HALL;
+			case PRESIDENTS_HALL:
+				currentLocation = PRESIDENTS_HALL;
+				break;
+			case NORTH:
+				currentLocation = GREAT_HALL;
+				break;
+			case GREAT_HALL:
+				currentLocation = GREAT_HALL;
+				break;
+			case WEST:
+				currentLocation = STUDY;
+				break;
+			case STUDY:
+				currentLocation = STUDY;
+				break;
+				/*
+			case BACK:
+				currentLocation = START;
+				break;
+				*/
+			default:
+				System.out.println("You can't get there yet!");
+		}
+		
 	}
 	
+	private void forestMove(String direction) {
+		switch (direction) {
+			case STAIRS:
+				currentLocation = ZEN_GARDEN;
+				break;
+			case BACK:
+				currentLocation = START;
+				break;
+		default:
+			System.out.println("???");
+		}
+
+	}
+
+	private void zenGMove(String direction) {
+		switch (direction) {
+		case BACK:
+			currentLocation = FOREST;
+			break;
+		default:
+			System.out.println("???");
+		}
+	}
+
+	
+	private void studyMove(String direction) {
+		switch (direction) {
+			case GREAT_HALL:
+				currentLocation = GREAT_HALL;
+				break;
+			case NORTH:
+				currentLocation = GREAT_HALL;
+				break;
+			case ROOM:
+				currentLocation = PRESIDENTS_HALL;
+				break;
+			case PRESIDENTS_HALL:
+				currentLocation = PRESIDENTS_HALL;
+				break;
+			case EAST:
+				currentLocation = PRESIDENTS_HALL;
+				break;
+			/*
+			case BACK:
+				currentLocation = MANSION;
+				break;
+				*/
+			default:
+				System.out.println("???");
+		}
+	}
+	
+	private void greatHMove(String direction) { 
+		switch (direction) {
+			case ROOM:
+				currentLocation = PRESIDENTS_HALL;
+				break;
+			case PRESIDENTS_HALL:
+				currentLocation = PRESIDENTS_HALL;
+				break;
+			case SOUTH:
+				currentLocation = MANSION;
+				break;
+			case STUDY:
+				currentLocation = STUDY;
+				break;
+			default:
+				System.out.println("???");
+		}
+	}
+	
+	private void presidentsHMove(String direction) {
+		switch (direction) {
+			case NORTH:
+				currentLocation = GREAT_HALL;
+				break;
+			case GREAT_HALL:
+				currentLocation = GREAT_HALL;
+				break;
+			case EAST:
+				currentLocation = STUDY;
+				break;
+			case STUDY:
+				currentLocation = STUDY;
+				break;
+			case STAIRS:
+				currentLocation = ART_MUSEUM;
+				break;
+			case DOWNSTAIRS:
+				currentLocation = ART_MUSEUM;
+				break;
+			default:
+				System.out.println("???");	
+		}
+	}
+	
+	private void museumMove(String direction) {
+		switch (direction) {
+			case WEST:
+				currentLocation = DUNGEON;
+				break;
+			case NORTH:
+				currentLocation = STORAGE;
+				break;
+			case BACK:
+				currentLocation = PRESIDENTS_HALL;
+				break;
+			case STAIRS:
+				currentLocation = PRESIDENTS_HALL;
+				break;
+			case UPSTAIRS:
+				currentLocation = PRESIDENTS_HALL;
+				break;
+			default:
+				System.out.println("???");
+		}
+	}
+	
+	private void storageMove(String direction) {
+		switch (direction) {
+			case EAST:
+				currentLocation = ART_MUSEUM;
+				break;
+			case SOUTH:
+				currentLocation = DUNGEON;
+				break;
+			default:
+				System.out.println("???");
+		}
+		
+	}
+	
+	private void dungeonMove(String direction) {
+		switch (direction) {
+			case EAST:
+				currentLocation = ART_MUSEUM;
+				break;
+			case NORTH:
+				currentLocation = DUNGEON;
+				break;
+			default:
+				System.out.println("???");
+		}
+		
+	}
+	
+	/*
+	 * 
+	
+	public void Start() {
+		System.out.println("You are standing on a brick path. In front of you is a \n"
+				+ "large mansion. To the east is a forest.");
+	}
+	public void Forest() {
+		System.out.println("You are in the forest. There is a set of stairs.");
+	}
+	
+	public void zenGarden() {
+		System.out.println("Welcome to the Zen Garden!! Calming music is playing.\n"
+						+ "There is a gentle stream flowing. And a bench. Have a rest.");
+	}
+	
+	public void Mansion() {
+		System.out.println("You have entered Bellarmine Mansion. To the east is a room. To the north"
+				+ "is the Great Hall. To the west is the President's Study.");
+	}
+	
+	public void Study() {
+		System.out.println("You are standing in the President's Study.");	
+	}
+	
+	public void greatHall() {
+		System.out.println("You are standing in the Great Hall.");
+	}
+	
+	public void hallOfPresidents() {
+		System.out.println("You are standing in the Hall of President's past. There are"
+				+ "dimly lit stairs leading to the basement.");
+	}
+	
+	public void artMuseum() {
+		System.out.println("You have entered the Art Museum. To your west "
+				+ "there are two rooms.");
+	}
+	
+	public void storageRoom() {
+		System.out.println("You are standing in the Storage Room.");	
+	}
+	
+	public void dungeon() {
+		System.out.println("YOU HAVE ENTERED THE DUNGEON. BEWARE.");
+	}
+	*/
 	
 	
 
