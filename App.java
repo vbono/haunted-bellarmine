cec2109d96ef66bd611a80e0ba41327d7b286e12
vbono <80070331+vbono@users.noreply.github.com>
2022-11-01 19:05:50 -0400

    Update App.java

--- App.java
+++ App.java
@@ -29,6 +29,7 @@ public class App {
 				+ "You are standing on a brick path. In front of you is a large mansion. To the east is a forest. \n"
 				+ "Type 'H' or 'Help' to get started.");
 		
+		// while score < 51
 		while(true) {
 			String command = s.nextLine();
 			
@@ -36,12 +37,39 @@ public class App {
 				Help.helpMe();
 			else if (command.equalsIgnoreCase("location"))
 				System.out.println(p1.getLocation());
+			else if (command.equalsIgnoreCase("inventory"))
+				p1.getInventory();
 			else if (command.equalsIgnoreCase("map"))
 				Help.map();
+			else if (command.equalsIgnoreCase("take") && p1.getLocation()=="zen garden") {
+				p1.addToInventory("wand");
+				p1.getScore("wand");
+			}
+			else if (command.equalsIgnoreCase("take") && p1.getLocation()=="study") {
+				p1.addToInventory("book");
+				p1.getScore("book");
+			}
+			else if (command.equalsIgnoreCase("use") && p1.getLocation()=="art museum" && p1.findItem("book") && p1.findItem("wand")) {
+				p1.addToInventory("rob"); 
+				p1.getScore("rob");
+			}
+			else if (command.equalsIgnoreCase("take") && p1.getLocation()=="storage room") {
+				p1.addToInventory("potion");
+				p1.getScore("potion");
+			}
+			else if (command.equalsIgnoreCase("take") && p1.getLocation()=="art museum" && p1.findItem("rob")) {
+				p1.addToInventory("sword");
+				p1.getScore("sword");
+			}
+			else if (command.equalsIgnoreCase("slay") && p1.getLocation()=="dungeon" && p1.findItem("potion") && p1.findItem("sword")) {
+				p1.addToInventory("slay");
+				p1.getScore("slay");
+			}
 			else
 				p1.move(command);
 		}
 		
+		// add prompts for items
 
 	}
 
