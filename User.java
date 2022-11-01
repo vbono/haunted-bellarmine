50919af6917bc0e3c74e91bca1b49a16139c6070
vbono <80070331+vbono@users.noreply.github.com>
2022-11-01 19:04:58 -0400

    Update User.java

--- User.java
+++ User.java
@@ -1,11 +1,18 @@
 package g1s1Package;
+import java.util.ArrayList;
 
 public class User {
 	private String name;
 	private Location loc;
+	private ScorableItem si;
+	private ArrayList<String> inventory;
+	private int score;
 	
 	public User() {
 		loc = new Location();
+		si = new ScorableItem();
+		inventory = new ArrayList<String>();
+		score = 0;
 	}
 	
 	public String getLocation() {
@@ -24,6 +31,27 @@ public class User {
 		this.name=name;
 	}
 	
+	public int getScore(String item) {
+		score += si.getItem(item);
+		return score;
+	}
+	
+	public void addToInventory(String item) {
+		inventory.add(item);
+	}
+	
+	public boolean findItem(String item) {
+		if (inventory.contains(item))
+			return true;
+		else
+			return false;
+	}
+	
+	public void getInventory() {
+		System.out.println(inventory);
+		
+	}
+	 	
 	
 	
 
