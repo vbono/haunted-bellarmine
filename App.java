55e27f86f9b847739a5be8a9a1bcf24560c859a4
amdidier <113067294+amdidier@users.noreply.github.com>
2022-11-01 20:58:44 -0400

    Update App.java

--- App.java
+++ App.java
@@ -37,36 +37,66 @@ public class App {
 				Help.helpMe();
 			else if (command.equalsIgnoreCase("location"))
 				System.out.println(p1.getLocation());
-			else if (command.equalsIgnoreCase("inventory"))
-				p1.getInventory();
 			else if (command.equalsIgnoreCase("map"))
 				Help.map();
+			else if (command.equalsIgnoreCase("inventory"))
+				System.out.println(p1.getInventory());
+			else if (command.equalsIgnoreCase("score"))
+				p1.printScore();
 			else if (command.equalsIgnoreCase("take") && p1.getLocation()=="zen garden") {
-				p1.addToInventory("wand");
 				p1.getScore("wand");
+				p1.addToInventory("wand");
+				p1.genericRetrieved();
 			}
 			else if (command.equalsIgnoreCase("take") && p1.getLocation()=="study") {
 				p1.addToInventory("book");
 				p1.getScore("book");
+				System.out.println("You have added this to your inventory! The book has a chapter \n"
+						+ "describing a spell that can be used to wake the ghost of Robert Bellarmine. \n"
+						+ "In case you come across him, say 'AWAKEN' to speak with his ghost.");
 			}
-			else if (command.equalsIgnoreCase("use") && p1.getLocation()=="art museum" && p1.findItem("book") && p1.findItem("wand")) {
-				p1.addToInventory("rob"); 
+			else if (command.equalsIgnoreCase("AWAKEN") && p1.getLocation()=="art museum" && p1.findItem("book") && p1.findItem("wand")) {
+				p1.addToInventory("rob");
 				p1.getScore("rob");
+				System.out.println("Robert Bellarmine's ghost floats out of the statue and tells you \n"
+						+ "that to defeat the beast in the dungeon, you will need protection. \n"
+						+ "There is a green potion you must drink to stay safe. Once you have had \n"
+						+ "the potion, Robert says he will give you the final tool you will need \n"
+						+ "to complete your quest.");
 			}
-			else if (command.equalsIgnoreCase("take") && p1.getLocation()=="storage room") {
+			else if (command.equalsIgnoreCase("drink") && p1.getLocation()=="storage room") {
 				p1.addToInventory("potion");
 				p1.getScore("potion");
 			}
 			else if (command.equalsIgnoreCase("take") && p1.getLocation()=="art museum" && p1.findItem("rob")) {
 				p1.addToInventory("sword");
 				p1.getScore("sword");
+				p1.genericRetrieved();
 			}
-			else if (command.equalsIgnoreCase("slay") && p1.getLocation()=="dungeon" && p1.findItem("potion") && p1.findItem("sword")) {
+			else if (command.equalsIgnoreCase("slay") && p1.getLocation()=="dungeon" && p1.findItem("sword") && p1.findItem("potion")) {
 				p1.addToInventory("slay");
 				p1.getScore("slay");
+				System.out.println("You have successfully slain the dragon! Congratulations!");
 			}
-			else
+			else {
 				p1.move(command);
+				
+				if (p1.getLocation()=="zen garden" && p1.findItem("wand")==false)
+					System.out.println("There is a wand lying on the bench. To pick it up, say 'take'.");
+				else if(p1.getLocation()=="study" && p1.findItem("book")==false)
+					System.out.println("There is a book sitting on the President's desk.");
+				else if(p1.getLocation()=="art museum" && p1.findItem("rob")==false)
+					System.out.println("There is a statue of Robert Bellarmine in the corner. To wake him, use the \n"
+							+ "spell that you read about in the book.");
+				else if(p1.getLocation()=="storage room" && p1.findItem("potion")==false)
+					System.out.println("There is a chest that looks like the one Robert Bellarmine described. \n"
+							+ "Inside the chest is a vial of a green liquid. Say 'drink' to take this potion.");
+				else if(p1.getLocation()=="art museum" && p1.findItem("sword")==false && p1.findItem("rob") && p1.findItem("potion"))
+					System.out.println("Robert Bellarmine presents a sword to aid you in your final quest. Good luck!"); 
+				else if(p1.getLocation()=="dungeon" && p1.findItem("slay")==false)
+					System.out.println("AHHH! There is a terrifying dragon in here!");
+			}
+			
 		}
 		
 		// add prompts for items
