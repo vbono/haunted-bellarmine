b156715cf7457c4340ac36f6e8b44e4071ee7d01
vbono <80070331+vbono@users.noreply.github.com>
2022-11-02 00:10:36 -0400

    while score<51, more print lines, quit option

--- App.java
+++ App.java
@@ -29,28 +29,35 @@ public class App {
 				+ "You are standing on a brick path. In front of you is a large mansion. To the east is a forest. \n"
 				+ "Type 'H' or 'Help' to get started.");
 		
-		// while score < 51
-		while(true) {
+		int score = p1.returnScore();
+		
+		while(score<51) {
 			String command = s.nextLine();
 			
 			if (command.equalsIgnoreCase("h") || command.equalsIgnoreCase("help"))
 				Help.helpMe();
+			else if (command.equalsIgnoreCase("quit")) {
+				System.out.println("Ok, see ya next time!");
+				break;
+			}
 			else if (command.equalsIgnoreCase("location"))
 				System.out.println(p1.getLocation());
 			else if (command.equalsIgnoreCase("map"))
 				Help.map();
 			else if (command.equalsIgnoreCase("inventory"))
-				System.out.println(p1.getInventory());
+				p1.getInventory();
 			else if (command.equalsIgnoreCase("score"))
 				p1.printScore();
 			else if (command.equalsIgnoreCase("take") && p1.getLocation()=="zen garden") {
 				p1.getScore("wand");
 				p1.addToInventory("wand");
 				p1.genericRetrieved();
+				score = p1.returnScore();
 			}
 			else if (command.equalsIgnoreCase("take") && p1.getLocation()=="study") {
 				p1.addToInventory("book");
 				p1.getScore("book");
+				score = p1.returnScore();
 				System.out.println("You have added this to your inventory! The book has a chapter \n"
 						+ "describing a spell that can be used to wake the ghost of Robert Bellarmine. \n"
 						+ "In case you come across him, say 'AWAKEN' to speak with his ghost.");
@@ -58,6 +65,7 @@ public class App {
 			else if (command.equalsIgnoreCase("AWAKEN") && p1.getLocation()=="art museum" && p1.findItem("book") && p1.findItem("wand")) {
 				p1.addToInventory("rob");
 				p1.getScore("rob");
+				score = p1.returnScore();
 				System.out.println("Robert Bellarmine's ghost floats out of the statue and tells you \n"
 						+ "that to defeat the beast in the dungeon, you will need protection. \n"
 						+ "There is a green potion you must drink to stay safe. Once you have had \n"
@@ -67,16 +75,20 @@ public class App {
 			else if (command.equalsIgnoreCase("drink") && p1.getLocation()=="storage room") {
 				p1.addToInventory("potion");
 				p1.getScore("potion");
+				score = p1.returnScore();
+				System.out.println("It tastes better than you think.");
 			}
 			else if (command.equalsIgnoreCase("take") && p1.getLocation()=="art museum" && p1.findItem("rob")) {
 				p1.addToInventory("sword");
 				p1.getScore("sword");
 				p1.genericRetrieved();
+				score = p1.returnScore();
 			}
 			else if (command.equalsIgnoreCase("slay") && p1.getLocation()=="dungeon" && p1.findItem("sword") && p1.findItem("potion")) {
 				p1.addToInventory("slay");
 				p1.getScore("slay");
-				System.out.println("You have successfully slain the dragon! Congratulations!");
+				score = p1.returnScore();
+				//break;
 			}
 			else {
 				p1.move(command);
@@ -92,12 +104,14 @@ public class App {
 					System.out.println("There is a chest that looks like the one Robert Bellarmine described. \n"
 							+ "Inside the chest is a vial of a green liquid. Say 'drink' to take this potion.");
 				else if(p1.getLocation()=="art museum" && p1.findItem("sword")==false && p1.findItem("rob") && p1.findItem("potion"))
-					System.out.println("Robert Bellarmine presents a sword to aid you in your final quest. Good luck!"); 
+					System.out.println("Robert Bellarmine presents a sword to aid you in your final quest. Take it from him. Good luck!"); 
 				else if(p1.getLocation()=="dungeon" && p1.findItem("slay")==false)
 					System.out.println("AHHH! There is a terrifying dragon in here!");
 			}
 			
 		}
+		if (score>50)
+			System.out.println("You have successfully slain the dragon! Congratulations!");
 		
 		// add prompts for items
 
