6127b13b370af51dc31c62543cb4a34ce49be201
amdidier <113067294+amdidier@users.noreply.github.com>
2022-12-05 15:23:40 -0500

    changed commandlist

--- Commands.java
+++ Commands.java
@@ -7,15 +7,19 @@ public enum Commands {
 	INVENTORY, MAP, SCORE, TAKE, AWAKEN, DRINK, SLAY;
 	
 	
-	public static ArrayList<Commands> CommandList() {
+	public static ArrayList<String> CommandList() {
 		ArrayList<Commands> CommandList = new ArrayList<>();
+		ArrayList<String> CommandList2 = new ArrayList<>();
 		
 		for (Commands c: Commands.values()) {
 			CommandList.add(c);
 		}
 		
-		return CommandList;
+		for (Commands c: CommandList) {
+			CommandList2.add(c.toString());
+		}
 		
+		return CommandList2;
 	}
 
 }
