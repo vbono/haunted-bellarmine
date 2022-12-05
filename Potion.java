2dbcfd17cd7248a5f45cc511d9006d11aa4179a8
amdidier <113067294+amdidier@users.noreply.github.com>
2022-12-05 13:30:36 -0500

    Create Potion.java

---
+++ Potion.java
@@ -0,0 +1,14 @@
+package milestone1package;
+
+public class Potion extends Object {
+	
+	public Potion(String name) {
+		super(name);
+	}
+	
+	
+	public void objectCollcted() {
+		System.out.println("It tastes better than you think.");
+	}
+
+}
