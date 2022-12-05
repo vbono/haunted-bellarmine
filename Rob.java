9fc86d50698972cf6d22af8da6ab78ba827d86b1
amdidier <113067294+amdidier@users.noreply.github.com>
2022-12-05 13:31:27 -0500

    Create Rob.java

---
+++ Rob.java
@@ -0,0 +1,18 @@
+package milestone1package;
+
+public class Rob extends Object {
+	
+	public Rob(String name) {
+		super(name);
+	}
+	
+	
+	public void objectCollcted() {
+		System.out.println("Robert Bellarmine's ghost floats out of the statue and tells you \n"
+				+ "that to defeat the beast in the dungeon, you will need protection. \n"
+				+ "There is a green potion you must drink to stay safe. Once you have had \n"
+				+ "the potion, Robert says he will give you the final tool you will need \n"
+				+ "to complete your quest.");
+
+	}
+}
