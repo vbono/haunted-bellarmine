82a10826d10754e9aeb23c0c5e063f14e3c0535f
vbono <80070331+vbono@users.noreply.github.com>
2022-12-05 19:36:27 -0500

    add constant interface

---
+++ ConstantInterface.java
@@ -0,0 +1,24 @@
+package g1s1Package;
+
+public interface ConstantInterface {
+	
+	public void startMessage();
+	public String getName();
+	public void welcome(String name);
+	public void quit(String name);
+	public void Start();
+	public void Forest();
+	public void zenGarden();
+	public void Mansion();
+	public void Study();
+	public void greatHall();
+	public void presidentsHall();
+	public void artMuseum();
+	public void artMuseum2();
+	public void storageRoom();
+	public void Dungeon();
+	public void nothing();
+	public void Default();
+	public void endMessage(String name);
+
+}
