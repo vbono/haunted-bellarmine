5756ba87bd1c27f0cf2f6bc5b8d21ed8c663eae9
vbono <80070331+vbono@users.noreply.github.com>
2022-12-05 19:38:00 -0500

    update print out

--- Book.java
+++ Book.java
@@ -8,8 +8,8 @@ public class Book extends Object {
 	
 	
 	public void objectCollcted() {
-		System.out.println("You have added this to your inventory! The book has a chapter \n"
-				+ "describing a spell that can be used to wake the ghost of Robert Bellarmine. \n"
+		System.out.println("You grabbed the book!\n"
+				+ "The book has a chapter describing a spell that can be used to wake the ghost of Robert Bellarmine. \n"
 				+ "In case you come across him, say 'AWAKEN' to speak with his ghost. \n"
 				+ "The book also tells you that if you say 'NAMASTE' when you're in the Great Hall, \n"
 				+ "you get transported to the zen garden, where something useful awaits. Give it a try!");
